7ab30a3b52232308f5d30e1dafca692281737eb4
Moyaccercchi <moya@asofterspace.com>
2018-03-29 13:12:41 +0000

    Get basic functionality to work after real test

--- main.js
+++ main.js
@@ -8,18 +8,19 @@ var fs = require('fs');
 
 function getCurrentChannelFile(channelId) {
 
-	var today = new Date();
-	var yyyy = today.getFullYear();
-	var mm = ('0' + (today.getMonth()+1)).slice(-2);
-	var dd = ('0' + today.getDate()).slice(-2);
+	var channelStr = "channel" + channelId;
 
-	var dateStr = dd+"-"+mm+"-"+yyyy;
-
-	if (!fs.existsSync(channelId)){
-		fs.mkdirSync(channelId);
+	if (!fs.existsSync(channelStr)){
+		fs.mkdirSync(channelStr);
 	}
 
-	return channelId + "/" + dateStr + ".txt";
+        var today = new Date();
+        var yyyy = today.getFullYear();
+        var mm = ('0' + (today.getMonth()+1)).slice(-2);
+        var dd = ('0' + today.getDate()).slice(-2);
+        var dateStr = dd+"-"+mm+"-"+yyyy;
+
+	return channelStr + "/" + dateStr + ".txt";
 }
 
 
@@ -42,18 +43,19 @@ server.get('/', function(req, res){
 // We are waiting for telegram to call us with a POST.
 server.post('/new-message', function(req, res) {
 
-	console.log("Telegram called us!");
+	if (req && req.body && req.body.message && req.body.message.chat &&
+	    req.body.message.chat.id && req.body.message.text) {
 
-	// req.body.chat.id contains the id
-	// req.body.text contains the actual message text
-	if (req.body && req.body.chat && req.body.chat.id && req.body.text) {
+		var channelId = req.body.message.chat.id;
+		var from = req.body.message.from.first_name;
+		var message = req.body.message.text;
 
-		var outFile = getCurrentChannelFile(req.body.chat.id);
+		var outFile = getCurrentChannelFile(channelId);
 
 		// We are using a flag for appending, not overwriting.
 		var logStream = fs.createWriteStream(outFile, {'flags': 'a'});
-		logStream.write("someone wrote:");
-		logStream.end(req.body.text);
+		logStream.write(from + ":\n");
+		logStream.end(message + "\n\n");
 	}
 
 	// Just return nothing - telegram does not care.
